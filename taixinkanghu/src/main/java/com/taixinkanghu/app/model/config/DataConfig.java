a6e4f3f6917585c63bf88594591d46b4f0afa890
wjygamedev <wjygamedev@126.com>
2015-07-19 01:51:06 +0800

    [add]添加陪护解析json的key

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/config/DataConfig.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/config/DataConfig.java
@@ -20,5 +20,25 @@ public class DataConfig
 
 	public final static String DHOSPITAL_ID = "id";
 	public final static String DHOSPITAL_NAME = "name";
+
+	public final static String NURSE_ID = "id";
+	public final static String NURSE_NAME = "name";
+	public final static String NURSE_AGE = "age";
+	public final static String NURSE_HOMETOWN = "hometown";
+	public final static String NURSE_NURING_EXP = "nursing_exp";
+	public final static String NURSE_NURING_LEVEL = "nursing_level";
+	public final static String NURSE_SERVICE_CHARGE_PER_DAY = "service_charge_per_day";
+	public final static String NURSE_SERVICE_STATUS = "nurse_service_status";
+
+	public final static String NURSE_JOB_NUM = "job_num";
+	public final static String NURSE_LANGUAGE_LEVEL = "language_level";
+	public final static String NURSE_EDUCATION = "education";
+	public final static String NURSE_NATION = "nation";
+	public final static String NURSE_INTRO = "intro";
+	public final static String NURSE_DEPARTMENTS = "departments";
+	public final static String NURSE_CERTIFICATE = "certificate";
+	public final static String NURSE_SERVICE_CONTENT = "service_content";
+
+
 }
 
