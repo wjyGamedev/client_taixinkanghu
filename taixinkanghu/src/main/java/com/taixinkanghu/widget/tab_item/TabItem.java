ad949f426049b48fb1d9810ed208b2a73d4a66e3
wjygamedev <wjygamedev@126.com>
2015-07-22 10:16:07 +0800

    [add]添加tab item类

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/widget/tab_item/TabItem.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.widget.tab_item.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/7/22		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.widget.tab_item;
+
+public class TabItem
+{ }
