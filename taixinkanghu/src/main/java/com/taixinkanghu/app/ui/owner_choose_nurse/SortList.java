309ab0b47e45b8ca783a6f7fb6eee6b7a81e2e9b
wjygamedev <wjygamedev@126.com>
2015-07-22 20:41:55 +0800

    [add] 01 添加我自己选护理员界面4个选项的处理类。

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/ui/owner_choose_nurse/SortList.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.ui.owner_choose_nurse.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/7/22		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.ui.owner_choose_nurse;
+
+import android.app.Activity;
+import android.view.View;
+
+import com.taixinkanghu.R;
+import com.taixinkanghu.app.ui.fragment.SelectSortFragment;
+import com.taixinkanghu.widget.drop_down_list.DropDownList;
+
+public class SortList extends DropDownList
+{
+	public SortList(Activity activity)
+	{
+		super(activity);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		m_activity.getFragmentManager().beginTransaction().addToBackStack(null).replace(R.id.title, new SelectSortFragment()).commit();
+	}
+}
