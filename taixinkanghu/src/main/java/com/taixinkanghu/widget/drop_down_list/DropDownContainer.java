84a2dd39522df883199fa72a37210252a0748c6f
wjygamedev <wjygamedev@126.com>
2015-07-23 12:04:12 +0800

    [add] 01 添加下来列表的相关类

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/widget/drop_down_list/DropDownContainer.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.widget.drop_down_list.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/7/23		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.widget.drop_down_list;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DropDownContainer extends Fragment
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		init();
+		initModule();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+	}
+
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+	}
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+	}
+
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+	}
+
+	@Override
+	public void onStop()
+	{
+		super.onStop();
+	}
+
+
+	public DropDownContainer()
+	{
+	}
+
+	private void init()
+	{
+
+	}
+
+	private void initModule()
+	{
+
+	}
+
+}
