c3442de13bb7b81e9619a1df15235456b361133b
wjygamedev <wjygamedev@126.com>
2015-07-27 21:51:45 +0800

    [add] 01. 添加预约陪护的新版本页面，输入被护理人信息。      包含空类和空layout文件。

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/ui/appointment_nursing/ApoitNursingActivity.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.ui.appointment_nursing.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/7/27		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.ui.appointment_nursing;
+
+public class ApoitNursingActivity
+{ }
