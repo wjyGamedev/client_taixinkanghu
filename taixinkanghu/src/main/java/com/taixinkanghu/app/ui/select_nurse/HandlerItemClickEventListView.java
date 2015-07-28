a6b1bbefec7e7b61933a3bb8f3bb226e8887a7da
wjygamedev <wjygamedev@126.com>
2015-07-29 03:01:59 +0800

    [add] 01. 添加选择护理员的listview的点击event

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/ui/select_nurse/HandlerItemClickEventListView.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.ui.select_nurse.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/7/29		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.ui.select_nurse;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.taixinkanghu.app.ui.activity.WorkerInfoActivity;
+
+public class HandlerItemClickEventListView implements AdapterView.OnItemClickListener
+{
+	private Context m_context = null;
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+	{
+		Activity activity = (Activity)m_context;
+		Intent intent = new Intent(activity, WorkerInfoActivity.class);
+		activity.startActivity(intent);
+	}
+
+	public HandlerItemClickEventListView(Activity activity)
+	{
+		m_context = activity;
+	}
+
+	public Context getContext()
+	{
+		return m_context;
+	}
+}
