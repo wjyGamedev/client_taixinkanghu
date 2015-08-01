f91bb03f533d0ba7d8a61f4ba12520d0dc64d3ef
wjygamedev <wjygamedev@126.com>
2015-08-01 13:53:06 +0800

    [modify]添加发送注册的方法，并调整代码格式。

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/NetService.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/NetService.java
@@ -26,11 +26,13 @@ import com.taixinkanghu.app.model.config.NetConfig;
 import com.taixinkanghu.app.model.net.event.ReqHospitalListEvent;
 import com.taixinkanghu.app.model.net.event.ReqNurseBasicListEvent;
 import com.taixinkanghu.app.model.net.event.ReqNurseSeniorListEvent;
+import com.taixinkanghu.app.model.net.event.ReqRegisterEvent;
 import com.taixinkanghu.app.model.net.exception.BaseErrorListener;
 import com.taixinkanghu.app.model.net.handler.ResHospitalListHandler;
 import com.taixinkanghu.app.model.net.handler.ResNurseBasicListHandler;
 import com.taixinkanghu.app.model.net.handler.ResNurseSeniorListHandler;
 import com.taixinkanghu.app.model.net.event.ReqShoppingBasicListEvent;
+import com.taixinkanghu.app.model.net.handler.ResRegisterHandler;
 import com.taixinkanghu.app.model.net.handler.ResShoppingBasicListHandler;
 import com.taixinkanghu.net.BaseHttp;
 
@@ -39,6 +41,19 @@ import de.greenrobot.event.EventBusException;
 
 public class NetService extends Service
 {
+	/**
+	 * 数据区
+	 */
+	private EventBus                    m_eventBus                    = null;//EventBus.getDefault();
+	private BaseErrorListener           m_baseErrorListener           = null;
+	private ResHospitalListHandler      m_resHospitalListHandler      = null;
+	private ResNurseBasicListHandler    m_resNurseBasicListHandler    = null;
+	private ResNurseSeniorListHandler   m_resNurseSeniorListHandler   = null;
+	private ResShoppingBasicListHandler m_resShoppingBasicListHandler = null;
+	private ResRegisterHandler m_resRegisterHandler = null;
+	private RequestQueue                m_requestQueue                = null;
+	private JsonObjectRequest           m_jsonObjectRequest           = null;
+
 	@Override
 	public void onCreate()
 	{
@@ -91,6 +106,7 @@ public class NetService extends Service
 		m_resNurseBasicListHandler = new ResNurseBasicListHandler();
 		m_resNurseSeniorListHandler = new ResNurseSeniorListHandler();
 		m_resShoppingBasicListHandler = new ResShoppingBasicListHandler();
+		m_resRegisterHandler = new ResRegisterHandler();
 		m_requestQueue = BaseHttp.getInstance().getRequestQueue();
 	}
 	private void initModule()
@@ -170,15 +186,18 @@ public class NetService extends Service
 		m_requestQueue.add(myReq);
 	}
 
-	/**
-	 * 数据区
-	 */
-	private EventBus                    m_eventBus                    = null;//EventBus.getDefault();
-	private BaseErrorListener           m_baseErrorListener           = null;
-	private ResHospitalListHandler      m_resHospitalListHandler      = null;
-	private ResNurseBasicListHandler    m_resNurseBasicListHandler    = null;
-	private ResNurseSeniorListHandler   m_resNurseSeniorListHandler   = null;
-	private ResShoppingBasicListHandler m_resShoppingBasicListHandler = null;
-	private RequestQueue                m_requestQueue                = null;
-	private JsonObjectRequest           m_jsonObjectRequest           = null;
+	//注册
+	public void onEventAsync(ReqRegisterEvent event)
+	{
+		String countryZipCode = event.getCountryZipCode();
+		String phoneNum = event.getPhoneNum();
+		String authCode = event.getAuthCode();
+		JsonObjectRequest myReq = new JsonObjectRequest(Request.Method.POST,
+														NetConfig.s_registerAddress,
+														null,
+														m_resRegisterHandler,
+														m_baseErrorListener);
+
+		m_requestQueue.add(myReq);
+	}
 }
