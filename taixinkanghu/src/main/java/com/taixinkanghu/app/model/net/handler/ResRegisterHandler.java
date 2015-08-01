e6ac031c8df3bfc98f7b2429a7af8456e09e6e9e
wjygamedev <wjygamedev@126.com>
2015-08-01 13:52:02 +0800

    [add]添加注册的时间和注册后的回调方法。

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/handler/ResRegisterHandler.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.model.net.handler.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/1		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.model.net.handler;
+
+import com.taixinkanghu.app.model.net.IResponseListener;
+
+import org.json.JSONObject;
+
+public class ResRegisterHandler extends IResponseListener
+{
+	@Override
+	public void onResponse(JSONObject response)
+	{
+		//TODO:注册返回信息
+	}
+}
