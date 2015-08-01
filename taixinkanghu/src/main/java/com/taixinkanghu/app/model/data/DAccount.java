9eb5d56629cea9b3fd308639ee4dcd5ff53e560c
wjygamedev <wjygamedev@126.com>
2015-08-01 14:06:47 +0800

    [add]添加注册的数据类

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DAccount.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.model.data.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/1		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.model.data;
+
+import android.util.Log;
+
+import com.taixinkanghu.app.model.config.DataConfig;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class DAccount
+{
+	private static DAccount s_dAccount = new DAccount();
+
+	private static String m_loginToken = null;
+
+	private DAccount()
+	{
+
+	}
+
+	public static DAccount GetInstance()
+	{
+		return s_dAccount;
+	}
+
+	public boolean serialization(JSONObject response)
+	{
+		JSONArray jsonArray = null;
+		try
+		{
+			m_loginToken = response.getString(DataConfig.REGISTER_TOKEN);
+		}
+		catch (JSONException e)
+		{
+			e.printStackTrace();
+			Log.e("error", e.getMessage().toString());
+			return false;
+		}
+
+		return  true;
+
+	}
+
+	public static String getM_loginToken()
+	{
+		return m_loginToken;
+	}
+}
