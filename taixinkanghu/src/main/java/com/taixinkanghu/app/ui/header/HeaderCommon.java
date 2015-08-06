7b5cb6413a40cea9c8ef89c831ad999b6159f188
wjygamedev <wjygamedev@126.com>
2015-08-06 14:48:00 +0800

    [modify] 01. 重构代码

--- taixinkanghu/src/main/java/com/taixinkanghu/app/ui/header/HeaderCommon.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/ui/header/HeaderCommon.java
@@ -15,7 +15,6 @@
 package com.taixinkanghu.app.ui.header;
 
 import android.app.Activity;
-import android.content.Context;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -24,25 +23,24 @@ import com.taixinkanghu.R;
 public class HeaderCommon
 {
 	//activity
-	private Context m_context = null;
+	private Activity     m_activity   = null;
 	//title
-	private TextView                 m_HeaderTV                 = null;
-	private ImageButton              m_BackImgBtn               = null;
+	private TextView    m_HeaderTV   = null;
+	private ImageButton m_BackImgBtn = null;
 	//logical
-	private boolean m_InitFlag = false;
+	private boolean     m_InitFlag   = false;
 
 	public HeaderCommon(Activity activity)
 	{
-		m_context = activity;
+		m_activity = activity;
 	}
 
 	public void init()
 	{
-		Activity activity = (Activity)m_context;
-		m_HeaderTV = (TextView)activity.findViewById(R.id.page_title);
-		m_BackImgBtn = (ImageButton)activity.findViewById(R.id.btn_back);
+		m_HeaderTV = (TextView)m_activity.findViewById(R.id.page_title);
+		m_BackImgBtn = (ImageButton)m_activity.findViewById(R.id.btn_back);
 
-		m_BackImgBtn.setOnClickListener(new HandlerClickEventBackBtn(activity));
+		m_BackImgBtn.setOnClickListener(new HandlerClickEventBackBtn(m_activity));
 		m_InitFlag = true;
 	}
 
