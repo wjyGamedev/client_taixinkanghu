84fe7ec54398349925822eabf19f36991139b4fb
wjygamedev <wjygamedev@126.com>
2015-08-06 11:15:15 +0800

    [modify] 01. 添加注册事件

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/handler/ResRegisterHandler.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/handler/ResRegisterHandler.java
@@ -16,15 +16,33 @@ package com.taixinkanghu.app.model.net.handler;
 
 import com.taixinkanghu.app.model.data.DAccount;
 import com.taixinkanghu.app.model.net.IResponseListener;
+import com.taixinkanghu.app.ui.register_page.DeserialFinishedEvent;
 
 import org.json.JSONObject;
 
+import de.greenrobot.event.EventBus;
+
 public class ResRegisterHandler extends IResponseListener
 {
+	private EventBus m_eventBus   = EventBus.getDefault();
+
+	public void init()
+	{
+	}
+
+	public void clearup()
+	{
+	}
+
+
 	@Override
 	public void onResponse(JSONObject response)
 	{
-		boolean bReturnFlag = DAccount.GetInstance().serialization(response);
-		//TODO:注册返回信息
+		boolean   bReturnFlag = DAccount.GetInstance().serialFromHttp(response);
+
+		//发送序列化完成event
+		DeserialFinishedEvent event = new DeserialFinishedEvent();
+		event.init(bReturnFlag);
+		m_eventBus.post(event);
 	}
 }
