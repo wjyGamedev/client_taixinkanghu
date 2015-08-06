52d0ec5f502c87424ca4ced2530c1d96c0103be2
wjygamedev <wjygamedev@126.com>
2015-08-06 15:50:52 +0800

    [add]添加逻辑基类event

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/event/logical/LogicalEvent.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.model.event.logical.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/6		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.model.event.logical;
+
+import com.taixinkanghu.app.model.event.BaseEvent;
+
+public class LogicalEvent extends BaseEvent
+{
+	public LogicalEvent(int iEventID)
+	{
+		super(iEventID);
+	}
+}
