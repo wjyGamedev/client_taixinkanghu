7cb8e0597226cdaf81fe92b697ba49a2c68df833
wjygamedev <wjygamedev@126.com>
2015-08-16 12:03:41 +0800

    [modify] 01. 重构

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DHospitalList.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DHospitalList.java
@@ -18,6 +18,7 @@ import com.taixinkanghu.R;
 import com.taixinkanghu.app.model.config.DataConfig;
 import com.taixinkanghu.app.model.exception.RuntimeExceptions.net.JsonSerializationException;
 import com.taixinkanghu.util.android.AppUtil;
+import com.taixinkanghu.util.logcal.LogicalUtil;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,6 +28,11 @@ import java.util.HashMap;
 
 public class DHospitalList
 {
+	private static DHospitalList           s_dHospitalList    = new DHospitalList();
+
+	private int    m_Status  = DataConfig.S_HTTP_OK;
+	private        HashMap<Integer, DHospital> m_dHospitalHashMap = new HashMap<Integer, DHospital>();
+
 	private DHospitalList()
 	{
 	}
@@ -38,12 +44,23 @@ public class DHospitalList
 
 	public boolean serialization(JSONObject response) throws JSONException
 	{
+		//01. 清空原来容器
 		if (m_dHospitalHashMap != null &&
 				m_dHospitalHashMap.size() != 0)
 		{
 			m_dHospitalHashMap.clear();
 		}
 
+		//02. http is ok
+		m_Status = response.getInt(DataConfig.STATUS_KEY);
+
+		if (!LogicalUtil.IsHttpSuccess(m_Status))
+		{
+			String errorMsg = response.getString(DataConfig.ERROR_MSG);
+			throw new JsonSerializationException(errorMsg);
+		}
+
+		//03. 序列化json
 		JSONArray jsonArray = response.getJSONArray(DataConfig.DHOSPITAL_LIST);
 		if (jsonArray == null)
 		{
@@ -66,6 +83,8 @@ public class DHospitalList
 
 	}
 
-	private static DHospitalList           s_dHospitalList    = new DHospitalList();
-	private        HashMap<Integer, DHospital> m_dHospitalHashMap = new HashMap<Integer, DHospital>();
+	public int getStatus()
+	{
+		return m_Status;
+	}
 }
