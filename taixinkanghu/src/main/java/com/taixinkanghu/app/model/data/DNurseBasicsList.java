ba1784bf990dc2b6e185526fc6aba63dae4f2f21
wjygamedev <wjygamedev@126.com>
2015-08-16 17:48:39 +0800

    [modify] 01. 将hashmap更改为arraylist 02. 添加httpstatus检测，并抛出异常

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DNurseBasicsList.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DNurseBasicsList.java
@@ -14,31 +14,47 @@
 
 package com.taixinkanghu.app.model.data;
 
+import com.taixinkanghu.R;
 import com.taixinkanghu.app.model.config.DataConfig;
+import com.taixinkanghu.app.model.exception.RuntimeExceptions.net.JsonSerializationException;
+import com.taixinkanghu.util.android.AppUtil;
+import com.taixinkanghu.util.logcal.LogicalUtil;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class DNurseBasicsList
 {
-	private HashMap<Integer, DNurseBasics> m_dNurseBasicsHashMap = new HashMap<Integer, DNurseBasics>();
 	private ArrayList<DNurseBasics>        m_nurseBasicses       = new ArrayList<>();
+	private int                         m_Status          = DataConfig.S_HTTP_OK;
 
 	public boolean serialization(JSONObject response) throws JSONException
 	{
 		if (m_nurseBasicses != null && m_nurseBasicses.isEmpty() == false)
 		{
-			m_dNurseBasicsHashMap.clear();
+			m_nurseBasicses.clear();
 		}
 
+		//02. http is ok
+		m_Status = response.getInt(DataConfig.STATUS_KEY);
+
+		if (!LogicalUtil.IsHttpSuccess(m_Status))
+		{
+			String errorMsg = response.getString(DataConfig.ERROR_MSG);
+			throw new JsonSerializationException(errorMsg);
+		}
+
+		//03. 序列化json
 		JSONArray jsonArray = response.getJSONArray(DataConfig.NURSE_BASICS_LIST);
 
 		if (jsonArray == null)
-			return false;
+		{
+			String errMsg = AppUtil.GetResources().getString(R.string.err_info_json_serilization);
+			throw new JsonSerializationException(errMsg + ":" + "nurse_basics_list");
+		}
 
 		JSONObject jsonObject = null;
 		DNurseBasics dNurseBasics = null;
@@ -54,10 +70,13 @@ public class DNurseBasicsList
 		return  true;
 	}
 
-	public HashMap<Integer, DNurseBasics> GetNurseBasicsHashMap()
+	public ArrayList<DNurseBasics> getNurseBasicses()
 	{
-		return m_dNurseBasicsHashMap;
+		return m_nurseBasicses;
 	}
 
-
+	public int getStatus()
+	{
+		return m_Status;
+	}
 }
