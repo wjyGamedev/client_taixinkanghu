8e6d135227ffbe82798340cecd1aae2d4808ff9f
wjygamedev <wjygamedev@126.com>
2015-08-16 11:50:06 +0800

    [modify] 01. 修改头文件

--- taixinkanghu/src/main/java/com/taixinkanghu/net/BaseHttp.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/net/BaseHttp.java
@@ -19,7 +19,7 @@ import android.content.Context;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
-import com.taixinkanghu.app.model.exception.RuntimeExceptions.NetRTException;
+import com.taixinkanghu.app.model.exception.RuntimeExceptions.net.NetRTException;
 
 public class BaseHttp
 {
