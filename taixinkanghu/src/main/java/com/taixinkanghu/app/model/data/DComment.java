69f66cb6c965fa1147ffaa80fc3744b4ac9f5283
wjygamedev <wjygamedev@126.com>
2015-08-17 14:16:19 +0800

    [add] 01. 添加评论类。

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DComment.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.model.data.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/17		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.model.data;
+
+public class DComment
+{ }
