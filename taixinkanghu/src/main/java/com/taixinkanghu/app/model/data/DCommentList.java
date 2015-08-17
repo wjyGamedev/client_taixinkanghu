2d22bdbbb646644108495bbfc50fb001b4501fed
wjygamedev <wjygamedev@126.com>
2015-08-17 14:21:38 +0800

    [modify] 01. 删除不使用的字段

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DCommentList.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DCommentList.java
@@ -14,37 +14,26 @@
 
 package com.taixinkanghu.app.model.data;
 
-import com.taixinkanghu.app.model.config.DataConfig;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DCommentList
 {
+	private int m_ID = 0;	//ID
+
 	public boolean serialization(JSONObject response) throws JSONException
 	{
-		m_iGoodRate = response.getInt(DataConfig.NURSE_GOOD_RATE);
-		m_iCommentNum = response.getInt(DataConfig.NURSE_COMMENT_NUM);
 		return true;
 	}
 
-	public void initID(int iID)
+	public void init(int iID)
 	{
-		m_iID = iID;
-	}
-	public int getiGoodRate()
-	{
-		return m_iGoodRate;
+		m_ID = iID;
 	}
 
-	public int getiCommentNum()
+	public int getID()
 	{
-		return m_iCommentNum;
+		return m_ID;
 	}
-
-	private int m_iID = 0;                   //ID
-	private int m_iGoodRate   = 0;    //好评率
-	private int m_iCommentNum = 0;//多少人进行了评论
-
 
 }
