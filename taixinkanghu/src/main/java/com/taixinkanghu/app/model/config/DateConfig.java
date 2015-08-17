443ca6b930f60a487ed45523d23453a981d617cb
wjygamedev <wjygamedev@126.com>
2015-08-17 14:15:51 +0800

    [add] 01. 按照业务逻辑添加消息config类

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/config/DateConfig.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.model.config.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/17		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.model.config;
+
+public class DateConfig
+{
+	//logical
+	//日期样式
+	public final static String PATTERN_DATE_MONTH_DAY_WEEK = "MM月dd日(E)";    //MM月dd日(星期E)
+	public final static String PATTERN_DATE_MONTH_DAY      = "MM月dd日";    //MM月dd日
+	public final static String PATTERN_DATE_YEAR_MONTH_DAY = "yyyy-MM-dd";    //YYYY年MM月dd日
+
+}
