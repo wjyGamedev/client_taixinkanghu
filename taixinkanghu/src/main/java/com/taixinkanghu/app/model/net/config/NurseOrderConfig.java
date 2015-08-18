56d1dd3075aa3e17eb8cd1d33836621a6db88bab
wjygamedev <wjygamedev@126.com>
2015-08-19 01:32:05 +0800

    [add] 01. 添加NurseOrderConfig

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/config/NurseOrderConfig.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.model.net.config.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/18		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.model.net.config;
+
+public class NurseOrderConfig
+{
+	//01. confirm
+	//0101. common
+
+
+	//0102. recv
+	//0103. send
+	public final static String NURSE_ID        = CommonConifg.ID;
+
+	public final static String SCHEDULE_ALL    = "schedule_all";
+	public final static String SCHEDULE_DAY    = "schedule_day";
+	public final static String SCHEDULE_NIGHT  = "schedule_night";
+
+	public final static String SERVICE_ADDRESS = "service_address";
+
+	public final static String NAME      = CommonConifg.NAME;
+	public final static String PHONE_NUM = "phone_num";
+	public final static String GENDER_ID = "gender_id";
+	public final static String AGE       = "age";
+	public final static String WEIGHT           = "weight";
+	public final static String HOSPITAL_ID      = "hospital_id";
+	public final static String DEPARTMENT_ID  = "department_id";
+	public final static String PATIENT_STATE_ID = "patient_state_id";
+
+	public final static String TOTAL_CHARGE = "total_charge";
+
+
+}
