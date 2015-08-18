2ec45a751c8886653138ad8bc15ba1a29e5d4c29
wjygamedev <wjygamedev@126.com>
2015-08-19 01:33:50 +0800

    [modify] 01. 反序列化成功后，发送ReqNurseSeniorListEvent消息，用来接收nurse senior list

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/handler/ResApoitNursingHandler.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/handler/ResApoitNursingHandler.java
@@ -14,14 +14,19 @@
 
 package com.taixinkanghu.app.model.net.handler;
 
+import com.taixinkanghu.app.model.data.DNurseBasics;
+import com.taixinkanghu.app.model.data.DNurseBasicsList;
 import com.taixinkanghu.app.model.data.DNurseContainer;
 import com.taixinkanghu.app.model.net.event.recv.FinishedNurseBasicListEvent;
 import com.taixinkanghu.app.model.exception.RuntimeExceptions.net.JsonSerializationException;
 import com.taixinkanghu.app.model.net.IResponseListener;
+import com.taixinkanghu.app.model.net.event.send.ReqNurseSeniorListEvent;
 import com.taixinkanghu.widget.dialog.register_page_dialog.RegisterDialog;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import de.greenrobot.event.EventBus;
 
@@ -32,6 +37,7 @@ public class ResApoitNursingHandler extends IResponseListener
 	@Override
 	public void onResponse(JSONObject response)
 	{
+		//01. 反序列化json
 		try
 		{
 			DNurseContainer.GetInstance().serialBasiclist(response);
@@ -49,9 +55,33 @@ public class ResApoitNursingHandler extends IResponseListener
 			return;
 		}
 
-		//解析成功，发送event
+		//02. 解析成功，发送event，到SelectNurseActivity
 		FinishedNurseBasicListEvent finishedNurseBasicListEvent = new FinishedNurseBasicListEvent();
 		m_eventBus.post(finishedNurseBasicListEvent);
+
+		//03. 发送event，请求nurse senior消息
+		ReqNurseSeniorListEvent reqNurseSeniorListEvent = new ReqNurseSeniorListEvent();
+
+		DNurseBasicsList nurseBasicsList = DNurseContainer.GetInstance().getNurseBasicsList();
+		ArrayList<DNurseBasics> nurseBasicsArrayList = nurseBasicsList.getNurseBasicses();
+		if (nurseBasicsArrayList == null)
+		{
+			RegisterDialog.GetInstance().setMsg("nurseBasicsArrayList == null");
+			RegisterDialog.GetInstance().show();
+			return;
+		}
+
+		ArrayList<Integer> nurseIDList = new ArrayList<>();
+		for (DNurseBasics nurseBasics : nurseBasicsArrayList)
+		{
+			if (nurseBasics == null)
+				continue;
+
+			nurseIDList.add(nurseBasics.getID());
+		}
+		reqNurseSeniorListEvent.setNurseIDList(nurseIDList);
+		m_eventBus.post(reqNurseSeniorListEvent);
+
 		return;
 
 	}
