e83f3fe8a5f899c3a7df46e41e07ce78741bab5c
wjygamedev <wjygamedev@126.com>
2015-08-18 11:16:26 +0800

    [modify] 01. 调整代码格式。 02. 添加对于异常的提示信息。

--- taixinkanghu/src/main/java/com/taixinkanghu/app/ui/nurse_order_confirm_page/HandlerClickEventNursOrderConfirm.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/ui/nurse_order_confirm_page/HandlerClickEventNursOrderConfirm.java
@@ -1,6 +1,7 @@
 package com.taixinkanghu.app.ui.nurse_order_confirm_page;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.view.View;
 
@@ -9,6 +10,7 @@ import com.taixinkanghu.app.ui.activity.AgreementActivity;
 import com.taixinkanghu.app.ui.appointment_nursing.ApoitNursingActivity;
 import com.taixinkanghu.app.ui.listener.view.BaseHandleOnClickEvent;
 import com.taixinkanghu.app.ui.nurs_order_pay_page.NursOrderPayActivity;
+import com.taixinkanghu.widget.dialog.register_page_dialog.RegisterDialog;
 
 /**
  * Created by Administrator on 2015/8/3.
@@ -23,39 +25,45 @@ public class HandlerClickEventNursOrderConfirm extends BaseHandleOnClickEvent
 	@Override
 	public void onClick(View v)
 	{
-		Activity activity = (Activity)m_context;
+		OrderConfirmActivity orderconfirmActivity = (OrderConfirmActivity)m_context;
+		if (orderconfirmActivity == null)
+		{
+			RegisterDialog.GetInstance().setMsg("activity == null");
+			RegisterDialog.GetInstance().show();
+			return;
+		}
+
 		switch (v.getId())
 		{
-			case R.id.btn_back:
+			case R.id.patient_state_region_ll:
 			{
-				activity.finish();
-				break;
+				FragmentTransaction transaction = orderconfirmActivity.getFragmentManager().beginTransaction();
+				SelectPatientStateFragment selectPatientStateFragment = new SelectPatientStateFragment();
+				transaction.replace(R.id.order_confirm_page, selectPatientStateFragment, selectPatientStateFragment.getClass().getName());
+				transaction.commit();
 			}
+			return;
+			case R.id.patient_region_ll:
+			{
+				Intent intent = new Intent(orderconfirmActivity, ApoitNursingActivity.class);
+				m_context.startActivity(intent);
+			}
+			return;
+			case R.id.user_protocol_tv:
+			{
+				Intent intent = new Intent(orderconfirmActivity, AgreementActivity.class);
+				m_context.startActivity(intent);
+			}
+			return;
 			case R.id.btn_pay:
 			{
-				Intent intent = new Intent(activity, NursOrderPayActivity.class);
+				Intent intent = new Intent(orderconfirmActivity, NursOrderPayActivity.class);
 				m_context.startActivity(intent);
-				break;
 			}
-			case R.id.user_protocol:
-			{
-				Intent intent = new Intent(activity, AgreementActivity.class);
-				m_context.startActivity(intent);
-				break;
-			}
-			case R.id.patient_region:
-			{
-				Intent intent = new Intent(activity, ApoitNursingActivity.class);
-				m_context.startActivity(intent);
-				break;
-			}
-			case R.id.patient_state_region:
-			{
-				activity.getFragmentManager().beginTransaction().addToBackStack(null).replace(R.id.order_confirm_page,
-																							  new SelectPatientStateFragment()
-																							 ).commit();
-				break;
-			}
+			return;
 		}
+		return;
 	}
+
+
 }
