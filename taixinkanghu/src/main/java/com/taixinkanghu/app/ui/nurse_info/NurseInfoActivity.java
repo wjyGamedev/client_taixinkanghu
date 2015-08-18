c403cfb5d3b306daa7ea32fa8c537689d0486d41
wjygamedev <wjygamedev@126.com>
2015-08-18 23:21:17 +0800

    [modify] 01. 修改接口getImgResIDbyIndex

--- taixinkanghu/src/main/java/com/taixinkanghu/app/ui/nurse_info/NurseInfoActivity.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/ui/nurse_info/NurseInfoActivity.java
@@ -225,7 +225,7 @@ public class NurseInfoActivity extends Activity
 
 		//02. set ui
 		//nurse basic
-		int iImageID = DFaceImages.getInstance().getImageIDbyIndex(0);
+		int iImageID = DFaceImages.getInstance().getImgResIDbyIndex(0);
 		m_circleImageView.setImageResource(iImageID);
 		m_nameTV.setText(m_nurseBasics.getName());
 		m_nuringLevelTV.setText(m_nurseBasics.getNursingLevel());
