c3e050b3b086be5b86fa709f352760f7ecd578fc
wjygamedev <wjygamedev@126.com>
2015-08-19 01:29:54 +0800

    [modify] 01. 修改正式流程。

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DScheduleList.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DScheduleList.java
@@ -14,27 +14,73 @@
 
 package com.taixinkanghu.app.model.data;
 
+import com.taixinkanghu.R;
+import com.taixinkanghu.app.model.config.DateConfig;
+import com.taixinkanghu.app.model.exception.RuntimeExceptions.net.JsonSerializationException;
+import com.taixinkanghu.app.model.net.config.NurseSeniorListConfig;
+import com.taixinkanghu.util.android.AppUtil;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DScheduleList
 {
-	private int m_ID = 0;	//ID
+	private ArrayList<Calendar> m_allCalendarList   = new ArrayList<>();
+	private ArrayList<Calendar> m_dayCalendarList   = new ArrayList<>();
+	private ArrayList<Calendar> m_nightCalendarList = new ArrayList<>();
+	private SimpleDateFormat    m_simpleDateFormat  = new SimpleDateFormat(DateConfig.PATTERN_DATE_YEAR_MONTH_DAY);
 
-	public boolean serialization(JSONObject response) throws JSONException
+	public boolean serialization(JSONObject response) throws JSONException, ParseException
 	{
+		JSONObject jsonDateList = response.getJSONObject(NurseSeniorListConfig.SCHEDULE_DATE_LIST);
+		if (jsonDateList == null)
+		{
+			String errMsg = AppUtil.GetResources().getString(R.string.err_info_json_serilization);
+			throw new JsonSerializationException(errMsg + ":" + NurseSeniorListConfig.SCHEDULE_DATE_LIST);
+		}
+
+		m_allCalendarList = serializeDate(jsonDateList, NurseSeniorListConfig.DATE_ALL_LIST);
+		m_dayCalendarList = serializeDate(jsonDateList, NurseSeniorListConfig.DATE_DAY_LIST);
+		m_nightCalendarList = serializeDate(jsonDateList, NurseSeniorListConfig.DATE_NIGHT_LIST);
+
 		return true;
 	}
 
-	public void init(int iID)
+	private ArrayList<Calendar> serializeDate(JSONObject jsonDateList, String key) throws JSONException, ParseException
 	{
-		m_ID = iID;
+		ArrayList<Calendar> calendarList = new ArrayList<>();
+		JSONArray jsonDateAllList = jsonDateList.getJSONArray(key);
+		String    strDate            = null;
+		for (int index = 0; index < jsonDateAllList.length(); ++index)
+		{
+			strDate = (String)jsonDateAllList.get(index);
+			Date date = m_simpleDateFormat.parse(strDate);
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(date);
+			calendarList.add(calendar);
+		}
+		return calendarList;
 	}
 
-	public int getID()
+	public ArrayList<Calendar> getAllCalendarList()
 	{
-		return m_ID;
+		return m_allCalendarList;
 	}
 
+	public ArrayList<Calendar> getDayCalendarList()
+	{
+		return m_dayCalendarList;
+	}
 
+	public ArrayList<Calendar> getNightCalendarList()
+	{
+		return m_nightCalendarList;
+	}
 }
