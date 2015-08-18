e9dcb4b6c15988c2bccd8c2926d930eaa89005e0
wjygamedev <wjygamedev@126.com>
2015-08-19 01:32:26 +0800

    [modify] 01. 添加字段

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/config/NurseSeniorListConfig.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/config/NurseSeniorListConfig.java
@@ -18,7 +18,8 @@ public class NurseSeniorListConfig
 {
 	//common
 	public final static String ID           = CommonConifg.ID;
-	//send(无)
+	//send
+	public final static String ID_LIST     = "id_list";
 	//recv
 	public final static String LIST     = "nurse_senior_list";
 	public final static String JOB_NUM         = "job_num";
@@ -29,6 +30,15 @@ public class NurseSeniorListConfig
 	public final static String DEPARTMENTS     = "departments";
 	public final static String CERTIFICATE     = "certificate";
 	public final static String SERVICE_CONTENT = "service_content";
+
+	public final static String SCHEDULE_DATE_LIST = "schedule";
+	public final static String DATE_ALL_LIST = "all";
+	public final static String DATE_DAY_LIST = "day";
+	public final static String DATE_NIGHT_LIST = "night";
+
 	public final static String GOOD_RATE       = "good_rate";
 	public final static String COMMENT_NUM     = "comment_num";
+	//logical
+	public final static String SPLIT    = ",";
+
 }
