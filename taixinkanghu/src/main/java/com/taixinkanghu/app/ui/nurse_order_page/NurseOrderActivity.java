faf90bb9ed376a85279c066a64feeb68f1d30697
wjygamedev <wjygamedev@126.com>
2015-08-20 14:25:07 +0800

    [add] 01. 添加nurse order activity类

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/ui/nurse_order_page/NurseOrderActivity.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.ui.nurse_order_page.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/19		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.ui.nurse_order_page;
+
+public class NurseOrderActivity
+{ }
