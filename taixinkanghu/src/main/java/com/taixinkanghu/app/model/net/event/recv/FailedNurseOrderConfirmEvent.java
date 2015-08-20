22a4d037f08a7a7a02abdbecefff51c760c64d8b
wjygamedev <wjygamedev@126.com>
2015-08-20 14:23:53 +0800

    [add] 01. 添加failed nurse order confirm event

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/event/recv/FailedNurseOrderConfirmEvent.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.model.net.event.recv.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/19		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.model.net.event.recv;
+
+import com.taixinkanghu.app.model.config.EventID;
+import com.taixinkanghu.app.model.net.event.BaseNetEvent;
+
+public class FailedNurseOrderConfirmEvent  extends BaseNetEvent
+{
+	public FailedNurseOrderConfirmEvent()
+	{
+		super(EventID.FAILED_NURSE_ORDER_CONFIRM);
+	}
+}
