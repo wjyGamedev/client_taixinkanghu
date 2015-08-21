0172eb88949c275405923c5857aadcfca3dee287
wjygamedev <wjygamedev@126.com>
2015-08-21 16:03:29 +0800

    [modify] 01. 添加SCHEDULE_SPLIT

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/config/DataConfig.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/config/DataConfig.java
@@ -30,6 +30,6 @@ public class DataConfig
 	public final static int SELECT_DAY_TYEP_NIGHT = 2;
 
 	public static final int DEFAULT_VALUE = -1;
-
+	public final static String SCHEDULE_SPLIT = ",";
 }
 
