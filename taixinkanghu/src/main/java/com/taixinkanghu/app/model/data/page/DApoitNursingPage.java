1621ec795a47429d9b482e311da5626341e89a09
wjygamedev <wjygamedev@126.com>
2015-08-22 04:44:42 +0800

    [modify] 01. 将DNursingDate拆出去。 02. 添加clearup方法 03. 添加成员级别的锁

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/page/DApoitNursingPage.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/page/DApoitNursingPage.java
@@ -15,305 +15,238 @@
 package com.taixinkanghu.app.model.data.page;
 
 import com.taixinkanghu.app.model.config.DataConfig;
-import com.taixinkanghu.app.model.config.DateConfig;
 import com.taixinkanghu.app.model.config.EnumConfig;
-import com.taixinkanghu.app.model.net.config.NurseBasicListConfig;
 
 import java.io.Serializable;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 
 public class DApoitNursingPage implements Serializable
 {
-	private static DApoitNursingPage s_dApoitNursingPage = new DApoitNursingPage();
-
 	//姓名
-	private String m_name = null;
+	private String m_name     = null;
+	private Object m_syncName = new Object();
 
 	//手机号码：
-	private String m_phone = null;
+	private String m_phone     = null;
+	private Object m_syncPhone = new Object();
 
 	//性别：男，女
-	private EnumConfig.SexType m_sexType = null;
+	private EnumConfig.SexType m_sexType    = null;
+	private Object             m_syncGender = new Object();
 
 	//年龄
-	private EnumConfig.AgeRage m_ageRage = null;
+	private EnumConfig.AgeRage m_ageRage     = null;
+	private Object             m_syncAgeRage = new Object();
 
 	//体重
-	private EnumConfig.WeightRage m_weightRage = null;
+	private EnumConfig.WeightRage m_weightRage     = null;
+	private Object                m_syncWeightRage = new Object();
 
 	//医院
-	private int m_hospitalID = -1;
+	private int    m_hospitalID     = DataConfig.DEFAULT_VALUE;
+	private Object m_syncHospitalID = new Object();
 
 	//科室
-	private int m_departmenetID = -1;
+	private int    m_departmenetID     = DataConfig.DEFAULT_VALUE;;
+	private Object m_syncDepartmenetID = new Object();
 
 	//病人状态
-	private EnumConfig.PatientState m_patientState = null;
+	private EnumConfig.PatientState m_patientState     = null;
+	private Object                  m_syncPatientState = new Object();
 
 	//护理时间
-	private DNursingDate m_nursingDate = null;
+	private DNursingDate m_nursingDate     = null;
+	private Object       m_syncNursingDate = new Object();
 
-	public static class DNursingDate
+	public void clearup()
 	{
-		private Date                          m_beingDate        = null;
-		private Date                          m_endDate          = null;
-		private ArrayList<ArrayList<Date>>    m_dateListAll      = new ArrayList<>();
-		private ArrayList<ArrayList<Integer>> m_typeListAll      = new ArrayList<>();
-		private String                        m_dateDescription  = null;
-		private SimpleDateFormat              m_simpleDateFormat = new SimpleDateFormat(DateConfig.PATTERN_DATE_YEAR_MONTH_DAY);
-
-		private int m_allNum   = 0;
-		private int m_dayNum   = 0;
-		private int m_nightNum = 0;
-
-		public DNursingDate(Date beingDate, Date endDate, ArrayList<ArrayList<Date>> dateListAll, ArrayList<ArrayList<Integer>>
-				typeListAll, String dateDescription)
+		synchronized (m_syncName)
 		{
-			m_beingDate = beingDate;
-			m_endDate = endDate;
-			m_dateListAll = dateListAll;
-			m_typeListAll = typeListAll;
-			m_dateDescription = dateDescription;
+			m_name = null;
 		}
 
-		public Date getBeingDate()
+		synchronized (m_syncPhone)
 		{
-			return m_beingDate;
+			m_phone = null;
 		}
 
-		public Date getEndDate()
+		synchronized (m_syncGender)
 		{
-			return m_endDate;
+			m_sexType = null;
 		}
 
-		public ArrayList<ArrayList<Date>> getDateListAll()
+		synchronized (m_syncAgeRage)
 		{
-			return m_dateListAll;
+			m_ageRage = null;
 		}
 
-		public ArrayList<ArrayList<Integer>> getTypeListAll()
+		synchronized (m_syncWeightRage)
 		{
-			return m_typeListAll;
+			m_weightRage = null;
 		}
 
-		public String getDateDescription()
+		synchronized (m_syncHospitalID)
 		{
-			return m_dateDescription;
+			m_hospitalID = DataConfig.DEFAULT_VALUE;
 		}
 
-		private String getDateDescription(int selectType)
+		synchronized (m_syncDepartmenetID)
 		{
-			if (m_dateListAll.size() != m_typeListAll.size())
-				return null;
-
-			String schedualDate = null;
-			String dateString   = null;
-			for (int iMonth = 0; iMonth < m_typeListAll.size(); iMonth++)
-			{
-				ArrayList<Integer> typeArrayList = m_typeListAll.get(iMonth);
-				ArrayList<Date> dateArrayList = m_dateListAll.get(iMonth);
-
-				if (m_typeListAll.size() != m_dateListAll.size())
-					return null;
-
-				for (int iDay = 0; iDay < typeArrayList.size(); ++iDay)
-				{
-					//DataConfig.SELECT_DAY_TYEP_ALL,SELECT_DAY_TYEP_DAY,SELECT_DAY_TYEP_NIGHT
-					if (typeArrayList.get(iDay) != selectType)
-						continue;
-
-					Date date = dateArrayList.get(iDay);
-					dateString = m_simpleDateFormat.format(date);
-					if (schedualDate == null)
-					{
-						schedualDate = (dateString + NurseBasicListConfig.SCHEDULE_SPLIT);
-					}
-					else
-					{
-						schedualDate += (dateString + NurseBasicListConfig.SCHEDULE_SPLIT);
-					}
-				}
-			}
-
-			return schedualDate;
+			m_departmenetID = DataConfig.DEFAULT_VALUE;
 		}
 
-
-		public String getSchedualAllDescription()
+		synchronized (m_syncPatientState)
 		{
-			return getDateDescription(DataConfig.SELECT_DAY_TYEP_ALL);
+			m_patientState = null;
 		}
 
-		public String getSchedualDayDescription()
+		synchronized (m_syncNursingDate)
 		{
-			return getDateDescription(DataConfig.SELECT_DAY_TYEP_DAY);
+			m_nursingDate = null;
 		}
-
-		public String getSchedualNightDescription()
-		{
-			return getDateDescription(DataConfig.SELECT_DAY_TYEP_NIGHT);
-		}
-
-		public int getAllNum()
-		{
-			if (m_allNum != 0)
-				return m_allNum;
-
-			for (ArrayList<Integer> integerList : m_typeListAll)
-			{
-				for (Integer integer : integerList)
-				{
-					if (integer == DataConfig.SELECT_DAY_TYEP_ALL)
-					{
-						m_allNum++;
-						continue;
-					}
-				}
-			}
-			return m_allNum;
-
-		}
-
-		public int getDayNum()
-		{
-			if (m_dayNum != 0)
-				return m_dayNum;
-
-			for (ArrayList<Integer> integerList : m_typeListAll)
-			{
-				for (Integer integer : integerList)
-				{
-					if (integer == DataConfig.SELECT_DAY_TYEP_DAY)
-					{
-						m_dayNum++;
-						continue;
-					}
-				}
-			}
-			return m_dayNum;
-		}
-
-		public int getNightNum()
-		{
-			if (m_nightNum != 0)
-				return m_nightNum;
-
-			for (ArrayList<Integer> integerList : m_typeListAll)
-			{
-				for (Integer integer : integerList)
-				{
-					if (integer == DataConfig.SELECT_DAY_TYEP_NIGHT)
-					{
-						m_nightNum++;
-						continue;
-					}
-				}
-			}
-			return m_nightNum;
-		}
-
-	}
-
-	public DApoitNursingPage()
-	{
-	}
-
-
-	public static DApoitNursingPage GetInstance()
-	{
-		return s_dApoitNursingPage;
 	}
 
 	//function:get/set
 	public String getName()
 	{
-		return m_name;
+		synchronized (m_syncName)
+		{
+			return m_name;
+		}
 	}
 
 	public void setName(String name)
 	{
-		m_name = name;
+		synchronized (m_syncName)
+		{
+			m_name = name;
+		}
 	}
 
 	public String getPhone()
 	{
-		return m_phone;
+		synchronized (m_syncPhone)
+		{
+			return m_phone;
+		}
 	}
 
 	public void setPhone(String phone)
 	{
-		m_phone = phone;
+		synchronized (m_syncPhone)
+		{
+			m_phone = phone;
+		}
 	}
 
 	public EnumConfig.SexType getSexType()
 	{
-		return m_sexType;
+		synchronized (m_syncGender)
+		{
+			return m_sexType;
+		}
 	}
 
 	public void setSexType(EnumConfig.SexType sexType)
 	{
-		m_sexType = sexType;
+		synchronized (m_syncGender)
+		{
+			m_sexType = sexType;
+		}
 	}
 
 	public EnumConfig.AgeRage getAgeRage()
 	{
-		return m_ageRage;
+		synchronized (m_syncAgeRage)
+		{
+			return m_ageRage;
+		}
 	}
 
 	public void setAgeRage(EnumConfig.AgeRage ageRage)
 	{
-		m_ageRage = ageRage;
+		synchronized (m_syncAgeRage)
+		{
+			m_ageRage = ageRage;
+		}
 	}
 
 	public EnumConfig.WeightRage getWeightRage()
 	{
-		return m_weightRage;
+		synchronized (m_syncWeightRage)
+		{
+			return m_weightRage;
+		}
 	}
 
 	public void setWeightRage(EnumConfig.WeightRage weightRage)
 	{
-		m_weightRage = weightRage;
+		synchronized (m_syncWeightRage)
+		{
+			m_weightRage = weightRage;
+		}
 	}
 
 	public int getHospitalID()
 	{
-		return m_hospitalID;
+		synchronized (m_syncHospitalID)
+		{
+			return m_hospitalID;
+		}
 	}
 
 	public void setHospitalID(int hospitalID)
 	{
-		m_hospitalID = hospitalID;
+		synchronized (m_syncHospitalID)
+		{
+			m_hospitalID = hospitalID;
+		}
 	}
 
 	public int getDepartmenetID()
 	{
-		return m_departmenetID;
+		synchronized (m_syncDepartmenetID)
+		{
+			return m_departmenetID;
+		}
 	}
 
 	public void setDepartmenetID(int departmenetID)
 	{
-		m_departmenetID = departmenetID;
+		synchronized (m_syncDepartmenetID)
+		{
+			m_departmenetID = departmenetID;
+		}
 	}
 
 	public EnumConfig.PatientState getPatientState()
 	{
-		return m_patientState;
+		synchronized (m_syncPatientState)
+		{
+			return m_patientState;
+		}
 	}
 
 	public void setPatientState(EnumConfig.PatientState patientState)
 	{
-		m_patientState = patientState;
+		synchronized (m_syncPatientState)
+		{
+			m_patientState = patientState;
+		}
 	}
 
 	public DNursingDate getNursingDate()
 	{
-		return m_nursingDate;
+		synchronized (m_syncNursingDate)
+		{
+			return m_nursingDate;
+		}
 	}
 
 	public void setNursingDate(DNursingDate nursingDate)
 	{
-		m_nursingDate = nursingDate;
+		synchronized (m_syncNursingDate)
+		{
+			m_nursingDate = nursingDate;
+		}
 	}
 }
