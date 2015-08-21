2431de392bf8ce34cbf0b369b3a8cefa96485652
wjygamedev <wjygamedev@126.com>
2015-08-21 16:24:56 +0800

    [add] 01. 添加护理模块，用来管理在nurse order 流程中的页面数据。

---
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/DNursingModule.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 213Team
+ *
+ * @className : com.taixinkanghu.app.model.data.${type_name}
+ * @version : 1.0.0
+ * @author : WangJY
+ * @description : ${TODO}
+ * <p/>
+ * Modification History:
+ * Date         	Author 		Version		Description
+ * ----------------------------------------------------------------
+ * 2015/8/21		WangJY		1.0.0		create
+ */
+
+package com.taixinkanghu.app.model.data;
+
+public class DNursingModule
+{
+	private static DNursingModule m_nursingModule = new DNursingModule();
+
+
+
+
+
+
+
+
+
+
+
+	private DNursingModule()
+	{
+	}
+
+	public static DNursingModule GetInstance()
+	{
+		return m_nursingModule;
+	}
+
+}
