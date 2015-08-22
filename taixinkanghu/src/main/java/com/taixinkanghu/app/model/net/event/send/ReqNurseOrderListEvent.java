e5de5a229554e39ab9418486cb1d4ce9497ce5f6
wjygamedev <wjygamedev@126.com>
2015-08-22 16:51:59 +0800

    [modify] 01. 添加用户ID，及序列化方式。

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/event/send/ReqNurseOrderListEvent.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/net/event/send/ReqNurseOrderListEvent.java
@@ -15,12 +15,34 @@
 package com.taixinkanghu.app.model.net.event.send;
 
 import com.taixinkanghu.app.model.config.EventID;
+import com.taixinkanghu.app.model.net.config.NurseOrderConfig;
 import com.taixinkanghu.app.model.net.event.BaseNetEvent;
+
+import java.util.HashMap;
 
 public class ReqNurseOrderListEvent extends BaseNetEvent
 {
+	private String m_userID = null;
+
 	public ReqNurseOrderListEvent()
 	{
 		super(EventID.QUEST_NURSE_ORDER_LIST);
+	}
+
+	public String getUserID()
+	{
+		return m_userID;
+	}
+
+	public void setUserID(String userID)
+	{
+		m_userID = userID;
+	}
+
+	public HashMap<String, String> getHashMap()
+	{
+		HashMap<String, String> nurseOrderList = new HashMap<String, String>();
+		nurseOrderList.put(NurseOrderConfig.USER_ID, m_userID);
+		return nurseOrderList;
 	}
 }
