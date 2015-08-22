498d31e16fc64536c98fc0b919ef414bc3005e27
wjygamedev <wjygamedev@126.com>
2015-08-22 16:51:17 +0800

    [modify] 01. 添加接口：传入listener 02. 增加两种按钮的方法，即传入listener

--- taixinkanghu/src/main/java/com/taixinkanghu/widget/dialog/register_page_dialog/RegisterDialog.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/widget/dialog/register_page_dialog/RegisterDialog.java
@@ -16,6 +16,7 @@ package com.taixinkanghu.widget.dialog.register_page_dialog;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 import com.taixinkanghu.R;
 import com.taixinkanghu.app.model.data.page.DGlobal;
@@ -58,6 +59,60 @@ public class RegisterDialog
 		if (m_builder != null)
 		{
 			m_builder.setPositiveButton(context.getResources().getString(R.string.info_i_known), null);
+			m_builder.setMessage(m_msg);
+		}
+	}
+
+	public void setMsg(String msg, Context context, DialogInterface.OnClickListener listener)
+	{
+		m_builder = new AlertDialog.Builder(context);
+		m_msg = msg;
+		if (m_builder != null)
+		{
+			m_builder.setPositiveButton(context.getResources().getString(R.string.info_i_known), listener);
+			m_builder.setMessage(m_msg);
+		}
+	}
+
+	public void setMsg(String msg, DialogInterface.OnClickListener listener)
+	{
+		Context context = DGlobal.GetInstance().getContext();
+		if (context == null)
+			return;
+
+		m_builder = new AlertDialog.Builder(context);
+		m_msg = msg;
+		if (m_builder != null)
+		{
+			m_builder.setPositiveButton(context.getResources().getString(R.string.info_i_known), listener);
+			m_builder.setMessage(m_msg);
+		}
+	}
+
+	public void setMsg(String msg, Context context, DialogInterface.OnClickListener confirmListener, DialogInterface.OnClickListener cancelListener)
+	{
+		m_builder = new AlertDialog.Builder(context);
+		m_msg = msg;
+		if (m_builder != null)
+		{
+			m_builder.setPositiveButton(context.getResources().getString(R.string.cancel_btn), cancelListener);
+			m_builder.setNegativeButton(context.getResources().getString(R.string.confirm_btn), confirmListener);
+			m_builder.setMessage(m_msg);
+		}
+	}
+
+	public void setMsg(String msg, DialogInterface.OnClickListener confirmListener, DialogInterface.OnClickListener cancelListener)
+	{
+		Context context = DGlobal.GetInstance().getContext();
+		if (context == null)
+			return;
+
+		m_builder = new AlertDialog.Builder(context);
+		m_msg = msg;
+		if (m_builder != null)
+		{
+			m_builder.setPositiveButton(context.getResources().getString(R.string.cancel_btn), cancelListener);
+			m_builder.setNegativeButton(context.getResources().getString(R.string.confirm_btn), confirmListener);
 			m_builder.setMessage(m_msg);
 		}
 	}
