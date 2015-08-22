192217031409f71eb78901004d9bd8ffa71b154a
wjygamedev <wjygamedev@126.com>
2015-08-22 16:44:00 +0800

    [modify] 01. 修改性别名称 02. 添加性别，患者状态，护工服务状态，订单状态的valueof方法。

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/config/EnumConfig.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/config/EnumConfig.java
@@ -20,15 +20,15 @@ import com.taixinkanghu.util.android.AppUtil;
 public class EnumConfig
 {
 	//01. 性别：男，女
-	public enum SexType {
-
-		MALE(AppUtil.GetResources().getString(R.string.content_male), 1),
-		FEMALE(AppUtil.GetResources().getString(R.string.content_female), 2);
+	public enum GenderStatus
+	{
+		MALE(AppUtil.GetResources().getString(R.string.gender_male), 1),
+		FEMALE(AppUtil.GetResources().getString(R.string.gender_female), 2);
 
 		private String m_name = null;
 		private int m_id = 1;
 
-		private SexType(String name, int id)
+		private GenderStatus(String name, int id)
 		{
 			m_name = name;
 			m_id = id;
@@ -50,6 +50,16 @@ public class EnumConfig
 			return (m_name + ":" + m_id);
 		}
 
+		public static GenderStatus valueOf(int id) {    //    手写的从int到enum的转换函数
+			switch (id) {
+			case 1:
+				return MALE;
+			case 2:
+				return FEMALE;
+			default:
+				return null;
+			}
+		}
 	}
 
 	//02. 年龄
@@ -155,6 +165,19 @@ public class EnumConfig
 		{
 			return ("[id:=" + m_id + "][name:" +m_name+"]");
 		}
+
+		public static PatientState valueOf(int id) {    //    手写的从int到enum的转换函数
+			switch (id) {
+			case 1:
+				return PATIENT_STATE_CARE_MYSELF;
+			case 2:
+				return PATIENT_STATE_HALF_CARE_MYSELF;
+			case 3:
+				return PATIENT_STATE_CANNT_CARE_MYSELF;
+			default:
+				return null;
+			}
+		}
 	}
 
 	//05. 订单状态
@@ -191,6 +214,72 @@ public class EnumConfig
 		{
 			return ("[id:=" + m_id + "][name:" +m_name+"]");
 		}
+
+		public static NurseOrderStatus valueOf(int id) {    //    手写的从int到enum的转换函数
+			switch (id) {
+			case 1:
+				return WAIT_PAYMENT;
+			case 2:
+				return WAIT_SERVICE;
+			case 3:
+				return IN_SERVICE;
+			case 4:
+				return FINISHED;
+			case 5:
+				return CANCELED;
+			case 6:
+				return WAIT_EVALUATION;
+			default:
+				return null;
+			}
+		}
 	}
+
+	//06. 护工服务状态
+	public enum NurseServiceStatus
+	{
+		FREE(AppUtil.GetResources().getString(R.string.nurse_service_status_free), 1),
+		IN_SERVICE(AppUtil.GetResources().getString(R.string.nurse_service_status_in_service), 2),
+		CANNT_SERVICE(AppUtil.GetResources().getString(R.string.nurse_service_status_cannt_service), 3);
+
+		private String m_name = null;
+		private int m_id = 0;
+
+		private NurseServiceStatus(String name, int id)
+		{
+			m_name = name;
+			m_id = id;
+		}
+
+		public String getName()
+		{
+			return m_name;
+		}
+
+		public int getId()
+		{
+			return m_id;
+		}
+
+		@Override
+		public String toString()
+		{
+			return ("[id:=" + m_id + "][name:" +m_name+"]");
+		}
+
+		public static NurseServiceStatus valueOf(int id) {    //    手写的从int到enum的转换函数
+			switch (id) {
+			case 1:
+				return FREE;
+			case 2:
+				return IN_SERVICE;
+			case 3:
+				return CANNT_SERVICE;
+			default:
+				return null;
+			}
+		}
+	}
+
 
 }
