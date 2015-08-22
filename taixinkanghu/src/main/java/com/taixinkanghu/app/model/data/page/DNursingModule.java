6c383d083ab50ef072b960dfb7bf5a31f70b7186
wjygamedev <wjygamedev@126.com>
2015-08-22 16:43:03 +0800

    [modify] 01. 添加成员变量DApoitNursing，DNurseOrderConfirm，DNurseOrderPay及其get接口

--- taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/page/DNursingModule.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/model/data/page/DNursingModule.java
@@ -18,7 +18,14 @@ public class DNursingModule
 {
 	private static DNursingModule m_nursingModule = new DNursingModule();
 
-	private DApoitNursingPage m_apoitNursingPage = new DApoitNursingPage();
+	private DApoitNursingPage m_apoitNursingPage      = new DApoitNursingPage();
+//	private Object m_syncDApoitNursingPage = new Object();
+
+	private DNurseOrderConfirmPage m_nurseOrderConfirmPage = new DNurseOrderConfirmPage();
+//	private Object m_syncDNurseOrderConfirmPage = new Object();
+
+	private DNurseOrderPayPage m_nurseOrderPayPage = new DNurseOrderPayPage();
+//	private Object m_syncDNurseOrderPayPage = new Object();
 
 
 	private DNursingModule()
@@ -28,6 +35,39 @@ public class DNursingModule
 	public static DNursingModule GetInstance()
 	{
 		return m_nursingModule;
+	}
+
+	public DApoitNursingPage getApoitNursingPage()
+	{
+		return m_apoitNursingPage;
+	}
+
+	public DNurseOrderConfirmPage getNurseOrderConfirmPage()
+	{
+		return m_nurseOrderConfirmPage;
+	}
+
+	public DNurseOrderPayPage getNurseOrderPayPage()
+	{
+		return m_nurseOrderPayPage;
+	}
+
+	public void clearup()
+	{
+		if (m_apoitNursingPage != null)
+		{
+			m_apoitNursingPage.clearup();
+		}
+
+		if (m_nurseOrderConfirmPage != null)
+		{
+			m_nurseOrderConfirmPage.clearup();
+		}
+
+		if (m_nurseOrderPayPage != null)
+		{
+			m_nurseOrderPayPage.clearup();
+		}
 	}
 
 }
