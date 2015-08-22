91704fafbe0c526cf4f28a01c97b98bc24cf959c
wjygamedev <wjygamedev@126.com>
2015-08-22 16:49:21 +0800

    [modify] 01. 添加取消支付方法 02. 修改FailedNurseOrderCheckEvent的处理接口 03. page date层级改变带来的改动。

--- taixinkanghu/src/main/java/com/taixinkanghu/app/ui/nurse_order_pay_page/NurseOrderPayActivity.java
+++ taixinkanghu/src/main/java/com/taixinkanghu/app/ui/nurse_order_pay_page/NurseOrderPayActivity.java
@@ -1,8 +1,11 @@
 package com.taixinkanghu.app.ui.nurse_order_pay_page;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -13,6 +16,7 @@ import com.taixinkanghu.app.model.data.net.DAccount;
 import com.taixinkanghu.app.model.data.page.DGlobal;
 import com.taixinkanghu.app.model.data.page.DNurseOrderConfirmPage;
 import com.taixinkanghu.app.model.data.page.DNurseOrderPayPage;
+import com.taixinkanghu.app.model.data.page.DNursingModule;
 import com.taixinkanghu.app.model.net.config.NurseOrderConfig;
 import com.taixinkanghu.app.model.net.event.recv.FailedNurseOrderCheckEvent;
 import com.taixinkanghu.app.model.net.event.recv.FinishNurseOrderAlipayEvent;
@@ -20,14 +24,13 @@ import com.taixinkanghu.app.model.net.event.recv.FinishedNurseOrderCheckEvent;
 import com.taixinkanghu.app.model.net.event.send.ReqNurseOrderAlipayEvent;
 import com.taixinkanghu.app.ui.appointment_nursing.ReqApoitNursingEvent;
 import com.taixinkanghu.app.ui.header.HeaderCommon;
+import com.taixinkanghu.app.ui.main_page.MainActivity;
 import com.taixinkanghu.app.ui.select_nurse.SelectNurseActivity;
 import com.taixinkanghu.third.party.alipay.Config;
+import com.taixinkanghu.third.party.alipay.PayResult;
 import com.taixinkanghu.third.party.alipay.Util;
 import com.taixinkanghu.third.party.alipay.UtilRSA;
 import com.taixinkanghu.widget.dialog.register_page_dialog.RegisterDialog;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -49,8 +52,10 @@ public class NurseOrderPayActivity extends Activity
 
 	//logical
 	private HandlerClickEventNursOrderPay m_handlerClickEventNursOrderPay = null;
+	private HandleClickEventOnDialog      m_handleClickEventOnDialog      = null;
+	private EventBus                      m_eventBus                      = EventBus.getDefault();
 
-	private EventBus m_eventBus = EventBus.getDefault();
+	private DNurseOrderPayPage m_nurseOrderPayPage = DNursingModule.GetInstance().getNurseOrderPayPage();
 
 
 	@Override
@@ -86,28 +91,56 @@ public class NurseOrderPayActivity extends Activity
 		super.onDestroy();
 	}
 
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		if (keyCode == KeyEvent.KEYCODE_BACK)
+		{
+			RegisterDialog.GetInstance().setMsg(getString(R.string.cancel_title), this, m_handleClickEventOnDialog, m_handleClickEventOnDialog);
+			RegisterDialog.GetInstance().show();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+
+	}
+
 	private void initData()
 	{
 		Intent intent = getIntent();
 		String userID = DAccount.GetInstance().getId();
-		DNurseOrderPayPage.GetInstance().setUserID(userID);
+
+		if (m_nurseOrderPayPage == null)
+		{
+			RegisterDialog.GetInstance().setMsg("m_nurseOrderPayPage == null", this);
+			RegisterDialog.GetInstance().show();
+			return;
+		}
+
+		m_nurseOrderPayPage.setUserID(userID);
 
 		int orderID = intent.getIntExtra(NurseOrderConfig.ORDER_ID, DataConfig.DEFAULT_VALUE);
 		if (orderID == DataConfig.DEFAULT_VALUE)
 		{
-			RegisterDialog.GetInstance().setMsg("nurseID is invalid[orderID:="+orderID+"]", this);
+			RegisterDialog.GetInstance().setMsg("nurseID is invalid[orderID:=" + orderID + "]", this);
 			RegisterDialog.GetInstance().show();
 			return;
 		}
-		DNurseOrderPayPage.GetInstance().setOrderID(orderID);
+		m_nurseOrderPayPage.setOrderID(orderID);
 
 	}
 
 	private void updateContent()
 	{
-		String orderSerialNum = DNurseOrderPayPage.GetInstance().getOrderSerialNum();
+		if (m_nurseOrderPayPage == null)
+		{
+			RegisterDialog.GetInstance().setMsg("m_nurseOrderPayPage == null", this);
+			RegisterDialog.GetInstance().show();
+			return;
+		}
+
+		String orderSerialNum = m_nurseOrderPayPage.getOrderSerialNum();
 		m_orderSerialNumTV.setText(orderSerialNum);
-		int totalPrice = DNurseOrderPayPage.GetInstance().getTotalPrice();
+		int totalPrice = m_nurseOrderPayPage.getTotalPrice();
 		m_priceTV.setText(String.valueOf(totalPrice));
 	}
 
@@ -136,6 +169,7 @@ public class NurseOrderPayActivity extends Activity
 		m_payBtn.setText(R.string.determine_pay_title);
 
 		m_handlerClickEventNursOrderPay = new HandlerClickEventNursOrderPay(this);
+		m_handleClickEventOnDialog = new HandleClickEventOnDialog();
 		m_eventBus.register(this);
 	}
 
@@ -150,15 +184,30 @@ public class NurseOrderPayActivity extends Activity
 	//下订单失败，护工在服务中。
 	public void onEventMainThread(FailedNurseOrderCheckEvent event)
 	{
-		//01. 清除下整个下订单流程中的数据
-		DNurseOrderConfirmPage.GetInstance().clearup();
-		DNurseOrderPayPage.GetInstance().clearup();
+		//01. 清除下订单流程中，order confirm，order  pay的数据
+		DNurseOrderConfirmPage nurseOrderConfirmPage = DNursingModule.GetInstance().getNurseOrderConfirmPage();
+		if (nurseOrderConfirmPage == null)
+		{
+			RegisterDialog.GetInstance().setMsg("nurseOrderConfirmPage == null", this);
+			RegisterDialog.GetInstance().show();
+			return;
+		}
+		DNurseOrderPayPage nurseOrderPayPage = DNursingModule.GetInstance().getNurseOrderPayPage();
+		if (nurseOrderPayPage == null)
+		{
+			RegisterDialog.GetInstance().setMsg("nurseOrderPayPage == null", this);
+			RegisterDialog.GetInstance().show();
+			return;
+		}
+		nurseOrderConfirmPage.clearup();
+		nurseOrderPayPage.clearup();
 
 		//02. flush nurse basic list event
 		ReqApoitNursingEvent reqApoitNursingEvent = new ReqApoitNursingEvent();
 		m_eventBus.post(reqApoitNursingEvent);
 
 		//03. 跳转到选择nurse页面
+		finish();
 		startActivity(new Intent(this, SelectNurseActivity.class));
 
 		return;
@@ -166,9 +215,16 @@ public class NurseOrderPayActivity extends Activity
 	//下订单成功，调用支付宝模块
 	public void onEventMainThread(FinishedNurseOrderCheckEvent event)
 	{
+		if (m_nurseOrderPayPage == null)
+		{
+			RegisterDialog.GetInstance().setMsg("m_nurseOrderPayPage == null", this);
+			RegisterDialog.GetInstance().show();
+			return;
+		}
+
 		//01. 获取order info
-		String orderID = DNurseOrderPayPage.GetInstance().getOrderID();
-		int totalPrice = DNurseOrderPayPage.GetInstance().getTotalPrice();
+		String orderID = m_nurseOrderPayPage.getOrderID();
+		int totalPrice = m_nurseOrderPayPage.getTotalPrice();
 		//String orderInfo = Util.GetNurseOrder(orderID, String.valueOf(totalPrice));
 		//测试
 		String orderInfo = Util.GetNurseOrder(orderID, "0.01");
@@ -199,46 +255,46 @@ public class NurseOrderPayActivity extends Activity
 	{
 		//01. 解析支付结果。
 		String result = event.getResult();
-		JSONObject jsonObj = null;
-		String resultStatus = null;
-		int successID = Config.PayStatus.PAY_STATUS_SUCCESS.getId();
-		int status = successID;
-		try
+		PayResult payResult = new PayResult(result);
+
+		// 支付宝返回此次支付结果及加签，建议对支付宝签名信息拿签约时支付宝提供的公钥做验签
+		String resultInfo = payResult.getResult();
+
+		String resultStatus = payResult.getResultStatus();
+
+		// 判断resultStatus 为“9000”则代表支付成功，具体状态码代表含义可参考接口文档
+		if (TextUtils.equals(resultStatus, String.valueOf(Config.PayStatus.PAY_STATUS_SUCCESS.getId())))
 		{
-			jsonObj = new JSONObject(result);
-			resultStatus = jsonObj.getString(NurseOrderConfig.RESULT_STATUS);
-			status = Integer.valueOf(resultStatus);
+//			RegisterDialog.GetInstance().setMsg(Config.PayStatus.PAY_STATUS_SUCCESS.getName(), this);
+//			RegisterDialog.GetInstance().show();
+			//继续进行，支付成功。
 		}
-		catch (JSONException e)
+		// 判断resultStatus 为非“9000”则代表可能支付失败
+		else
 		{
-			RegisterDialog.GetInstance().setMsg(e.toString(), this);
-			RegisterDialog.GetInstance().show();
-			return;
-		}
-		catch (NumberFormatException e)
-		{
-			RegisterDialog.GetInstance().setMsg(e.toString(), this);
-			RegisterDialog.GetInstance().show();
-			return;
+			// “8000”代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
+			if (TextUtils.equals(resultStatus, String.valueOf(Config.PayStatus.PAY_STATUS_PAY_RESULT_WAIT_CONFIRM.getId())))
+			{
+				RegisterDialog.GetInstance().setMsg(Config.PayStatus.PAY_STATUS_PAY_RESULT_WAIT_CONFIRM.getName(), this);
+				RegisterDialog.GetInstance().show();
+				//继续进行，等待异步结果。
+			}
+			// 其他值就可以判断为支付失败，包括用户主动取消支付，或者系统返回的错误
+			else
+			{
+				//支付失败，则允许继续进行支付。
+				RegisterDialog.GetInstance().setMsg(getResources().getString(R.string.pay_status_failed) , this);
+				RegisterDialog.GetInstance().show();
+				return;
+			}
 		}
 
-		//02. 支付失败
-		if (status != successID)
-		{
-			//TODO:打印log
-		}
-
-		//03. 支付成功
-		//0301. 清除交易流程中的数据
-		//0302. 跳转到主页面。
-
+		//03. 支付成功,
+		paySuccessAction();
 
 		return;
 
 	}
-
-
-
 
 	/**
 	 * alipay
@@ -254,5 +310,48 @@ public class NurseOrderPayActivity extends Activity
 	}
 
 
+	/**
+	 * dialog listener
+	 */
+	public class HandleClickEventOnDialog implements DialogInterface.OnClickListener
+	{
+		@Override
+		public void onClick(DialogInterface dialog, int which)
+		{
+			//01. 确认，走取消支付流程。
+			if (which == DialogInterface.BUTTON_POSITIVE)
+			{
+				cancelPayAction();
+			}
+			//02. 取消,关闭弹出对话框。
+			else if (which == DialogInterface.BUTTON_NEGATIVE)
+			{
+				dialog.dismiss();
+			}
+			return;
+		}
+	}
+
+	/**
+	 * action
+	 */
+	private void cancelPayAction()
+	{
+		//01. 清除交易流程中的数据
+		DNursingModule.GetInstance().clearup();
+
+		//02. 跳转到主页面。
+		finish();
+		startActivity(new Intent(this, MainActivity.class));
+	}
+	private void paySuccessAction()
+	{
+		//01. 清除交易流程中的数据
+		DNursingModule.GetInstance().clearup();
+
+		//02. 跳转到主页面。
+		finish();
+		startActivity(new Intent(this, MainActivity.class));
+	}
 
 }
